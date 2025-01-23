04ba69c6793d17b93a409cff66d1a5f52042e6d9
Divyanshu Sinha <115927018+divyanshu1906@users.noreply.github.com>
2025-01-23 23:09:38 +0530

    Time: 19 ms (15.75%) | Memory: 26.4 MB (83.73%) - LeetSync

--- 1396-count-servers-that-communicate/count-servers-that-communicate.cpp
+++ 1396-count-servers-that-communicate/count-servers-that-communicate.cpp
@@ -3,25 +3,38 @@ public:
     int countServers(vector<vector<int>>& grid) {
         int n = grid.size();
         int m = grid[0].size();
-        vector<int> rowCount(n, 0), colCount(m, 0);
-        int totalServers = 0;
+        queue<pair<int, int>> q;
+        int ans = 0;
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
                 if (grid[i][j] == 1) {
-                    rowCount[i]++;
-                    colCount[j]++;
-                    totalServers++;
+                    q.push({i, j});
                 }
             }
         }
-        int connectedServers = 0;
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (grid[i][j] == 1 && (rowCount[i] > 1 || colCount[j] > 1)) {
-                    connectedServers++;
+        while (!q.empty()) {
+            pair<int, int> front = q.front();
+            q.pop();
+            int i = front.first;
+            int j = front.second;
+
+            int count = 0; 
+            for (int i_ = 0; i_ < n; i_++) {
+                if(i==i_)continue;
+                if (grid[i_][j] == 1) {
+                    count++;
                 }
             }
+            for (int j_ = 0; j_ < m; j_++) {
+                if(j==j_)continue;
+                if (grid[i][j_] == 1) {
+                    count++;
+                }
+            }
+            if (count > 0) {
+                ans++;
+            }
         }
-        return connectedServers;
+        return ans;
     }
 };
