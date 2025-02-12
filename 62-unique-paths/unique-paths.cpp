e4a8e808240946d83a13d13329943e4a2218edc6
Divyanshu Sinha <115927018+divyanshu1906@users.noreply.github.com>
2025-02-12 21:21:10 +0530

    Time: 2 ms (18.49%) | Memory: 9.4 MB (33.59%) - LeetSync

--- 62-unique-paths/unique-paths.cpp
+++ 62-unique-paths/unique-paths.cpp
@@ -15,10 +15,12 @@ public:
         return dp[row][col] = sum;
     }
     int uniquePaths(int m, int n) {
-        initialRow = m;
-        initialCol = n;
-        vector<vector<int>>dp(m, vector<int>(n, -1));
-        vector<pair<int, int>>directions = {{0, -1}, {-1, 0}};
-        return dfs(m-1, n-1, directions, dp);
+        vector<vector<int>>dp(m, vector<int>(n, 1));
+        for(int i=1; i<m; i++){
+            for(int j=1; j<n; j++){
+                dp[i][j] = dp[i-1][j] + dp[i][j-1];
+            }
+        }
+        return dp[m-1][n-1];
     }
 };
