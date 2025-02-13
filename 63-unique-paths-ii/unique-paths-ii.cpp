1bf8d5bb5a822e3f28bd55dcff1ecfaa08db5dde
Divyanshu Sinha <115927018+divyanshu1906@users.noreply.github.com>
2025-02-13 17:01:27 +0530

    Time: 0 ms (100.00%) | Memory: 11.9 MB (54.59%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.cpp
+++ 63-unique-paths-ii/unique-paths-ii.cpp
@@ -1,23 +1,9 @@
 class Solution {
 public:
-    vector<vector<int>>directions = {{-1, 0}, {0, -1}};
-    int n, m;
-    int dfs(int row, int col, vector<vector<int>>& obstacleGrid, vector<vector<int>>&dp){
-        if(row==0 && col==0) return 1;
-        int sum = 0;
-        if(dp[row][col] != -1) return dp[row][col];
-        for(auto &dir : directions){
-            int row_ = row + dir[0];
-            int col_ = col + dir[1];
-            if(row_>=0 && row_<n && col_>=0 && col_<m && obstacleGrid[row_][col_]!=1){
-                sum += dfs(row_, col_, obstacleGrid, dp);
-            }
-        }
-        return dp[row][col] = sum;
-    }
     int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
-        n = obstacleGrid.size();
-        m = obstacleGrid[0].size();
+        int n = obstacleGrid.size();
+        int m = obstacleGrid[0].size();
+        vector<vector<int>>directions = {{-1, 0}, {0, -1}};
         if(obstacleGrid[n-1][m-1]==1) return 0;
         vector<vector<int>>dp(n, vector<int>(m, 0));
         dp[0][0] = 1;
