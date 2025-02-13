fe992b319755e61f33b667170fdf2e7fb4153508
Divyanshu Sinha <115927018+divyanshu1906@users.noreply.github.com>
2025-02-13 17:00:42 +0530

    Time: 0 ms (100.00%) | Memory: 11.8 MB (54.59%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.cpp
+++ 63-unique-paths-ii/unique-paths-ii.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
-    int n, m;
     vector<vector<int>>directions = {{-1, 0}, {0, -1}};
+    int n, m;
     int dfs(int row, int col, vector<vector<int>>& obstacleGrid, vector<vector<int>>&dp){
         if(row==0 && col==0) return 1;
         int sum = 0;
@@ -19,7 +19,22 @@ public:
         n = obstacleGrid.size();
         m = obstacleGrid[0].size();
         if(obstacleGrid[n-1][m-1]==1) return 0;
-        vector<vector<int>>dp(n, vector<int>(m, -1));
-        return dfs(n-1, m-1, obstacleGrid, dp);
+        vector<vector<int>>dp(n, vector<int>(m, 0));
+        dp[0][0] = 1;
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+            int sum = 0;
+            if(i==0 && j==0) continue;
+                for(auto &dir : directions){
+                    int row_ = i + dir[0];
+                    int col_ = j + dir[1];
+                    if(row_>=0 && row_<n && col_>=0 && col_<m && obstacleGrid[row_][col_]!=1){
+                        sum += dp[row_][col_];
+                    }
+                }
+                dp[i][j] = sum;
+            }
+        }
+        return dp[n-1][m-1];
     }
 };
