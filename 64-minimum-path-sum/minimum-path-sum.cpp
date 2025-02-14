d1accafcd42af2db561249e54b1adfd77066bf60
Divyanshu Sinha <115927018+divyanshu1906@users.noreply.github.com>
2025-02-14 16:35:35 +0530

    Time: 6 ms (6.72%) | Memory: 16.5 MB (14.17%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.cpp
+++ 64-minimum-path-sum/minimum-path-sum.cpp
@@ -6,14 +6,12 @@ public:
         if(row==0 && col==0) return grid[row][col];
         if(dp[row][col] != -1) return dp[row][col];
         int minSum = INT_MAX;
-        int sum = INT_MAX;
         for(auto &dir : directions){
             int row_ = row + dir[0];
             int col_ = col + dir[1];
             if(row_>=0 && row_<n && col_>=0 && col_<m){
-                sum = grid[row][col] + dfs(row_, col_, grid, dp);
+                minSum = min(minSum , grid[row][col] + dfs(row_, col_, grid, dp));
             }
-            minSum = min(minSum, sum);
         }
         return dp[row][col]=minSum;
     }
