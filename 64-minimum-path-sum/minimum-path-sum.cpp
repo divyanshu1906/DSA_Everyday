4bee9ace8e56db7f29e2ca47bdabf9c57cb804bb
Divyanshu Sinha <115927018+divyanshu1906@users.noreply.github.com>
2025-02-14 16:49:31 +0530

    Time: 0 ms (100.00%) | Memory: 16.5 MB (5.29%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.cpp
+++ 64-minimum-path-sum/minimum-path-sum.cpp
@@ -1,23 +1,9 @@
 class Solution {
 public:
-    int n, m;
-    vector<vector<int>>directions = {{0, -1}, {-1, 0}};
-    int dfs(int row, int col, vector<vector<int>>& grid, vector<vector<int>>&dp){
-        if(row==0 && col==0) return grid[row][col];
-        if(dp[row][col] != -1) return dp[row][col];
-        int minSum = INT_MAX;
-        for(auto &dir : directions){
-            int row_ = row + dir[0];
-            int col_ = col + dir[1];
-            if(row_>=0 && row_<n && col_>=0 && col_<m){
-                minSum = min(minSum , grid[row][col] + dfs(row_, col_, grid, dp));
-            }
-        }
-        return dp[row][col]=minSum;
-    }
     int minPathSum(vector<vector<int>>& grid) {
-        n = grid.size();
-        m = grid[0].size();
+        int n = grid.size();
+        int m = grid[0].size();
+        vector<vector<int>>directions = {{0, -1}, {-1, 0}};
         vector<vector<int>>dp(n, vector<int>(m));
         dp[0][0] = grid[0][0];
         for(int i=0; i<n; i++){
