1231eec1c864f12122ca1ff3430d88f7b1726d41
Divyanshu Sinha <115927018+divyanshu1906@users.noreply.github.com>
2025-02-14 15:51:42 +0530

    Time: 18 ms (76.65%) | Memory: 78 MB (56.86%) - LeetSync

--- 1477-product-of-the-last-k-numbers/product-of-the-last-k-numbers.cpp
+++ 1477-product-of-the-last-k-numbers/product-of-the-last-k-numbers.cpp
@@ -1,20 +1,33 @@
 class ProductOfNumbers {
 public:
     vector<int>ans;
+    int n;
     ProductOfNumbers() {
-        // ans.clear();
+        ans.clear();
+        n=0;
     }
     
     void add(int num) {
-        ans.push_back(num);
+        if(num==0){
+            ans = {};
+            n=0;
+        }
+        else{
+            if(!ans.empty()){
+                ans.push_back(ans[n-1]*num);
+            }
+            else {
+                ans.push_back(num);
+            }
+            n++;
+        }
     }
     
     int getProduct(int k) {
-        int product = 1;
-        for(int i = ans.size()-k; i<ans.size(); i++){
-            product *= ans[i];
-        }
-        return product;
+        if(k>n) return 0;
+        else if(k==n) return ans[n-1];
+        
+        return ans[n-1]/ans[n-k-1];
     }
 };
 
