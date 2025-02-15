ee99da1909ca2084443a0b056c20b4b2d323ab28
Divyanshu Sinha <115927018+divyanshu1906@users.noreply.github.com>
2025-02-15 15:25:53 +0530

    Time: 759 ms (10.33%) | Memory: 538.8 MB (7.81%) - LeetSync

--- 2802-find-the-punishment-number-of-an-integer/find-the-punishment-number-of-an-integer.cpp
+++ 2802-find-the-punishment-number-of-an-integer/find-the-punishment-number-of-an-integer.cpp
@@ -1,28 +1,29 @@
 class Solution {
 public:
-    bool recursive(int i, int currSum, string s, int num){
+    bool recursive(int i, int currSum, string s, int num, vector<vector<int>>&dp){
         if(i==s.length()){
             return currSum==num;
         }
         if(currSum > num) return false;
-
+        if(dp[i][currSum] != -1) return dp[i][currSum];
         bool possible = false;
         for(int j=i; j<s.length(); j++){
             string str = s.substr(i, j-i+1);
             int val = stoi(str);
-            possible = possible || recursive(j+1, currSum+val, s, num);
+            possible = possible || recursive(j+1, currSum+val, s, num, dp);
             if(possible){
                 return true;
             }
         }
-        return possible;
+        return dp[i][currSum] = possible;
     }
     int punishmentNumber(int n) {
-        int punish = 0;
+       int punish = 0;
         for(int num=1; num<=n; num++){
             int sq = num*num;
             string s = to_string(sq);
-            if(recursive(0, 0, s, num)){
+            vector<vector<int>>dp(s.length(), vector<int>(num+1, -1));
+            if(recursive(0, 0, s, num, dp)){
                 punish += sq;
             }
         }
