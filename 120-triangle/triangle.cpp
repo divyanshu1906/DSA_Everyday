c87da7dd768f2c62ccebe92db4fdb46c034d980c
Divyanshu Sinha <115927018+divyanshu1906@users.noreply.github.com>
2025-02-18 16:19:07 +0530

    Time: 0 ms (100.00%) | Memory: 13.3 MB (27.41%) - LeetSync

--- 120-triangle/triangle.cpp
+++ 120-triangle/triangle.cpp
@@ -1,15 +1,18 @@
 class Solution {
 public:
-    int findingMinimumSum(int i, int j, int n, vector<vector<int>>& triangle, vector<vector<int>>&dp){
-        if(i==n-1) return triangle[i][j];
-        if(dp[i][j] != -1) return dp[i][j];
-        int down = triangle[i][j] + findingMinimumSum(i + 1, j, n, triangle, dp);
-        int diagonal = triangle[i][j] + findingMinimumSum(i+1, j+1, n, triangle, dp);
-        return dp[i][j] = min(down, diagonal);
-    }
     int minimumTotal(vector<vector<int>>& triangle) {
         int n = triangle.size();
-        vector<vector<int>>dp(n, vector<int>(n, -1));
-        return findingMinimumSum(0, 0, n, triangle, dp);
+        vector<vector<int>>dp(n, vector<int>(n, 0));
+        for(int j=0; j<n; j++){
+            dp[n-1][j] = triangle[n-1][j];
+        }
+        for(int i = n-2; i>=0; i--){
+            for(int j = i; j>=0; j--){
+                int down = triangle[i][j] + dp[i+1][j];
+                int diagonal = triangle[i][j] + dp[i+1][j+1];
+                dp[i][j] = min(down, diagonal);
+            }
+        }
+        return dp[0][0];
     }
 };
