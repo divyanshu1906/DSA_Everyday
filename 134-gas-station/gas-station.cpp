542ddb5cbfa4f3cc57b8f37ec90935bf10aa3c08
Divyanshu Sinha <115927018+divyanshu1906@users.noreply.github.com>
2026-01-16 23:08:51 +0530

    Time: 4 ms (24.72%) | Memory: 112.5 MB (24.41%) - LeetSync

--- 134-gas-station/gas-station.cpp
+++ 134-gas-station/gas-station.cpp
@@ -2,43 +2,27 @@ class Solution {
 public:
     int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
         int n = gas.size();
+        int totalGas = 0, totalCost = 0;
 
-        vector<int>startingPoint;
         for(int i=0; i<n; i++){
-            if(gas[i] >= cost[i]){
-                startingPoint.push_back(i);
+            totalGas  += gas[i];
+            totalCost += cost[i];
+        }
+
+        if(totalCost > totalGas) return -1;
+
+        int currentGas = 0;
+        int start = 0;
+
+        for(int i=0; i<n; i++){
+            currentGas += gas[i] - cost[i];
+
+            if(currentGas < 0){
+                currentGas = 0;
+                start = i+1;
             }
         }
 
-        if(startingPoint.empty()) return -1;
-
-        int ans = -1;
-
-        int m = startingPoint.size();
-        for(int i=0; i<m; i++){
-            int idx = startingPoint[i];
-
-            int currGas = gas[idx];
-            int currCost = cost[idx];
-
-            int totalGas = currGas - currCost;
-            int start = (idx + 1) % n;
-
-            while(start != idx && totalGas > 0){
-                int currGas = gas[start];
-                int currCost = cost[start];
-
-                totalGas = totalGas + currGas - currCost;
-
-                start = (start + 1) % n;
-            }
-
-            if(start == idx && totalGas >= 0){
-                ans = idx;
-                break;
-            }
-        }
-
-        return ans; 
+        return start;
     }
 };
