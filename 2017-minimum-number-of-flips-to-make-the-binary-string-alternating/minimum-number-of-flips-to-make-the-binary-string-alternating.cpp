79dd308a1edb777c7aefbc3f7c257f918ec766e4
Divyanshu Sinha <115927018+divyanshu1906@users.noreply.github.com>
2026-03-07 18:20:22 +0530

    Time: 16 ms (62.00%) | Memory: 14.3 MB (93.60%) - LeetSync

--- 2017-minimum-number-of-flips-to-make-the-binary-string-alternating/minimum-number-of-flips-to-make-the-binary-string-alternating.cpp
+++ 2017-minimum-number-of-flips-to-make-the-binary-string-alternating/minimum-number-of-flips-to-make-the-binary-string-alternating.cpp
@@ -3,27 +3,24 @@ public:
     int minFlips(string s) {
         int n = s.size();
 
-        s = (s+s);
-
-        string s1, s2;
-
-        for(int i=0; i<n*2; i++){
-            s1 += (i%2==0 ? '1' : '0');
-            s2 += (i%2==0 ? '0' : '1');
-        }
-
         int i = 0, j=0;
         int flip1 = 0, flip2 = 0;
 
         int result = INT_MAX;
 
         while(j<2*n){
-            if(s[j] != s1[j]) flip1++;
-            if(s[j] != s2[j]) flip2++;
+            char expectedChar1 = (j%2==0 ? '1' : '0');
+            char expectedChar2 = (j%2==0 ? '0' : '1');
+
+            if(s[j%n] != expectedChar1) flip1++;
+            if(s[j%n] != expectedChar2) flip2++;
 
             if(j-i+1 > n){
-                if(s[i] != s1[i]) flip1--;
-                if(s[i] != s2[i]) flip2--;
+                expectedChar1 = (i%2==0 ? '1' : '0');
+                expectedChar2 = (i%2==0 ? '0' : '1');
+                
+                if(s[i%n] != expectedChar1) flip1--;
+                if(s[i%n] != expectedChar2) flip2--;
                 i++;
             }
 
