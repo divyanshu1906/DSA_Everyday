28f52ffe44355f3e0531563693784e20a0b13ced
Divyanshu Sinha <115927018+divyanshu1906@users.noreply.github.com>
2026-03-08 15:29:24 +0530

    Time: 4 ms (14.83%) | Memory: 30.1 MB (42.16%) - LeetSync

--- 1658-minimum-swaps-to-arrange-a-binary-grid/minimum-swaps-to-arrange-a-binary-grid.cpp
+++ 1658-minimum-swaps-to-arrange-a-binary-grid/minimum-swaps-to-arrange-a-binary-grid.cpp
@@ -2,25 +2,34 @@ class Solution {
 public:
     int minSwaps(vector<vector<int>>& grid) {
         int n = grid.size();
-        vector<int> zeros(n);
 
-        for (int i = 0; i < n; i++) {
-            int count = 0;
-            for (int j = n - 1; j >= 0 && grid[i][j] == 0; j--) count++;
-            zeros[i] = count;
+        vector<int>rowZeroCount(n,0);
+
+        for(int i=0; i<n; i++){
+            int continuousZero = 0;
+            for(int j=n-1; j>=0; j--){
+                if(grid[i][j] == 1) break;
+                continuousZero++;
+            }
+            rowZeroCount[i] = continuousZero;
         }
 
         int swaps = 0;
 
-        for (int i = 0; i < n; i++) {
-            int needed = n - i - 1;
-            int j = i;
-            while (j < n && zeros[j] < needed) j++;
-            if (j == n) return -1;
-            while (j > i) {
-                swap(zeros[j], zeros[j - 1]);
-                j--;
+        for(int i=0;i<n;i++){
+            int required = n-i-1;
+
+            int j=i;
+            while(j<n && rowZeroCount[j] < required){
+                j++;
+            }
+
+            if(j==n) return -1;
+
+            while(j>i){
                 swaps++;
+                swap(rowZeroCount[j], rowZeroCount[j-1]);
+                j--;
             }
         }
 
