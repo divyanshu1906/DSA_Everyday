65b8308a8ffcb4d01bf924b2b50d536fa46ff756
Divyanshu Sinha <115927018+divyanshu1906@users.noreply.github.com>
2026-03-27 22:15:46 +0530

    Time: 0 ms (100.00%) | Memory: 31.4 MB (49.69%) - LeetSync

--- 3215-matrix-similarity-after-cyclic-shifts/matrix-similarity-after-cyclic-shifts.cpp
+++ 3215-matrix-similarity-after-cyclic-shifts/matrix-similarity-after-cyclic-shifts.cpp
@@ -4,22 +4,12 @@ public:
         int m = mat.size();
         int n = mat[0].size();
 
-        vector<vector<int>>rotatedMat(m, vector<int>(n, 0));
-
-        int idxChange = k%n;
-
         for(int i=0; i<m; i++){
             for(int j=0; j<n; j++){
-                if(i%2==0){
-                    int j_ = ((j-idxChange) + n)%n;
-                    rotatedMat[i][j_] = mat[i][j];
-                }else{
-                    int j_ = (j+idxChange) % n;
-                    rotatedMat[i][j_] = mat[i][j]; 
-                }
+                if(mat[i][(j+k) % n] != mat[i][j]) return false;
             }
         }
 
-        return rotatedMat == mat;
+        return true;
     }
 };
