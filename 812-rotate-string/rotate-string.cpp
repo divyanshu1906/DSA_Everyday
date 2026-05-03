d16a196dd9e1fb94235e25a347b02a8d9bfc7b00
Divyanshu Sinha <115927018+divyanshu1906@users.noreply.github.com>
2026-05-03 14:07:47 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (24.74%) - LeetSync

--- 812-rotate-string/rotate-string.cpp
+++ 812-rotate-string/rotate-string.cpp
@@ -1,22 +1,60 @@
 class Solution {
 public:
+    vector<int>lpsBuilder(string &goal){
+        int n = goal.size();
+        vector<int>lps(n, 0);
+        int i=1;
+        int length = 0;
+
+        while(i<n){
+            if(goal[i] == goal[length]){
+                length++;
+                lps[i] = length;
+                i++;
+            }
+            else {
+                if(length != 0){
+                    length = lps[length-1];
+                }
+                else{
+                    lps[i] = 0;
+                    i++;
+                }
+            }
+        }
+
+        return lps;
+    }
     bool rotateString(string s, string goal) {
         if(s.size() != goal.size()) return false;
 
-        string newStr = s + s;
+        vector<int>lps = lpsBuilder(goal);
 
-        for(int i=0; i<newStr.size(); i++){
-            int j=0;
-            int k=i;
+        string newS = s+s;
+        int n = newS.size();
+        int m = goal.size();
 
-            while(j<goal.size() && goal[j] == newStr[k]){
+        int i=0, j=0;
+
+        while(i<n && j<m){
+            bool flag = false;
+            if(newS[i] == goal[j]){
                 j++;
-                k++;
+                i++;
             }
-
-            if(j==goal.size()) return true;
+            else if(j!=0){
+                j = lps[j-1];
+                flag = true;
+            }
+            else{
+                j=0;
+                i++;
+            }
         }
 
+        if(j==m) return true;
+        
         return false;
     }
+
 };
